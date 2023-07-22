f6add959e91da75c4db9dab86f32a5c9b954a339
DEBAMRITA11 <amritadeb440@gmail.com>
2023-07-23 02:44:38 +0600

    hero-start

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Hero = () => {
+    return (
+        <section className='hero-area'>
+         <div className='hero-content'>
+
+         </div>
+        </section>
+    );
+};
+
+export default Hero;

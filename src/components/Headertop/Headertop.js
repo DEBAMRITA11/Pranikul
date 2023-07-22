c03965158c1d871c09c132e9ddba58e7935ae6fe
DEBAMRITA11 <amritadeb440@gmail.com>
2023-07-22 23:08:28 +0600

    header-top

--- src/components/Headertop/Headertop.js
+++ src/components/Headertop/Headertop.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Logo from "../../images/pow.png";
+import Clock from '../../images/clock.png';
 import "./Headertop.css";
 
 const Headertop = () => {
     return (
-        <div>
-            <h5>this is headertop area </h5>
+        <div className='header-top-area d-flex align-items container justify-between'>
+            <div className='logo d-flex p-2 mb-3'>
+            <img src={Logo} alt=''/>
+            <h1>Pranikul</h1>
+            </div>
+            <div className='header-content d-flex p-2'>
+            <img src={Clock} alt=''/> 
+            <h5>Open today at 10:00 - 16:00</h5>
+            <button className='btn-button'>Buy Tickt</button> 
+            </div>
+            
         </div>
     );
 };

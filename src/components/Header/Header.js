38c99c4e8215d5535a77c5bfeac54ceecf2d2a98
DEBAMRITA11 <amritadeb440@gmail.com>
2023-07-22 21:29:58 +0600

    yout text

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Headertop from "../Headertop/Headertop";
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <header className='header-area'>
+         <Headertop />
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Headertop/Headertop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Headertop.css";
+
+const Headertop = () => {
+    return (
+        <div>
+            <h5>this is headertop area </h5>
+        </div>
+    );
+};
+
+export default Headertop;
